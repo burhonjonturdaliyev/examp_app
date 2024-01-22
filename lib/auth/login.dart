74d1abe9e80c4af9db97405c48af6986444555a2
Burkhonjon Turdialiev <burhonjonturdaliyev2003@gmail.com>
2024-01-22 14:54:28 +0500

    Fixing

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool show = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,7 +57,85 @@ class LoginPage extends StatelessWidget {
                       color: Colors.white,
                       fontWeight: FontWeight.bold),
                 ),
-              )
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                onTapOutside: (event) => FocusScope.of(context).unfocus(),
+                style: const TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                    label: const Text("enter_your_username"),
+                    prefixIcon: const Icon(
+                      Icons.person,
+                      color: Colors.white,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(13),
+                    )),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                onTapOutside: (event) => FocusScope.of(context).unfocus(),
+                obscureText: show,
+                style: const TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                    label: const Text("enter the password"),
+                    prefixIcon: const Icon(
+                      Icons.password_sharp,
+                      color: Colors.white,
+                    ),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          show = !show;
+                        });
+                      },
+                      icon: const Icon(
+                        Icons.remove_red_eye,
+                        color: Colors.white,
+                      ),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(13),
+                    )),
+              ),
+              const SizedBox(height: 5),
+              const Align(
+                alignment: Alignment.topRight,
+                child: Text(
+                  "Forget Password",
+                  style: TextStyle(color: Colors.red),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      maximumSize: const Size(double.infinity, 50),
+                      minimumSize: const Size(double.infinity, 50),
+                      backgroundColor: Colors.red,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12))),
+                  onPressed: () {},
+                  child: const Text(
+                    "Continue",
+                    style: TextStyle(color: Colors.white),
+                  )),
+              const SizedBox(
+                height: 10,
+              ),
+              RichText(
+                  text: const TextSpan(children: [
+                TextSpan(
+                    text: "Don't have a accaount? ",
+                    style: TextStyle(color: Colors.white)),
+                TextSpan(
+                    text: "SignUp ", style: TextStyle(color: Colors.white)),
+              ]))
             ],
           ),
         ),
